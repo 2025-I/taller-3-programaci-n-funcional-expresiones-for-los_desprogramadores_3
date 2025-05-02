ecf78861e5345a216af820848bf4e6bbd2169459
bypipe <felipe.calan@correounivalle.edu.co>
2025-05-02 18:14:49 -0500

    segundo commit con avances

--- app/src/test/scala/taller/TestAplicarMovimiento.scala
+++ app/src/test/scala/taller/TestAplicarMovimiento.scala
@@ -38,6 +38,33 @@ class TestAplicarMovimiento extends AnyFunSuite {
     val e5 = obj.aplicarMovimiento(e4, Uno(-2))
     assert(e5 == (List('a','b','c','d'), List(), List()))
   }
+
+  test("Prueba7 - inversión completa de un tren de 4 vagones") {
+    val obj = new Maniobra()
+    val e1 = (List('a', 'b', 'c', 'd'), List(), List())
+    val e2 = obj.aplicarMovimiento(e1, Uno(1) )
+    assert(e2 == (List('a','b','c'), List('d'), List()))
+    val e3 = obj.aplicarMovimiento(e2, Dos(3) )
+    assert(e3 == (List(), List('d'), List('a','b','c')))
+    val e4 = obj.aplicarMovimiento(e3, Uno(-1) )
+    assert(e4 == (List('d'), List(), List('a','b','c')))
+    val e5 = obj.aplicarMovimiento(e4, Dos(-3) )
+    assert(e5 == (List('d','a','b','c'), List(), List()))
+    val e6 = obj.aplicarMovimiento(e5, Uno(1))
+    assert(e6 == (List('d','a','b'), List('c'), List()))
+    val e7 = obj.aplicarMovimiento(e6, Dos(2))
+    assert(e7 == (List('d'), List('c'), List('a','b')))
+    val e8 = obj.aplicarMovimiento(e7, Uno(-1))
+    assert(e8 == (List('d','c'), List(), List('a','b')))
+    val e9 = obj.aplicarMovimiento(e8, Dos(-1))
+    assert(e9 == (List('d','c','a'), List(), List('b')))
+    val e10 = obj.aplicarMovimiento(e9, Uno(1))
+    assert(e10 == (List('d','c'), List('a'), List('b')))
+    val e11 = obj.aplicarMovimiento(e10, Dos(-1))
+    assert(e11 == (List('d','c','b'), List('a'), List()))
+    val e12 = obj.aplicarMovimiento(e11, Uno(-1))
+    assert(e12 == (List('d','c','b','a'), List(), List()))
+  }
 }
 
 
